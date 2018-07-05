1ade9349e3d1c6defc4769eabcc3b7f0b463bbef
salanho <espiritosanto.gabriel@gmail.com>
2018-07-05 09:04:58 -0300

    backoffice e users controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  get 'backoffice' => 'backoffice#mybackoffice'
+  get 'backoffice' => 'backoffice#backoffice'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
