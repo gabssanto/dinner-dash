2512859008ef4b1bc28e44d75e2f4dcf56b39bc5
salanho <espiritosanto.gabriel@gmail.com>
2018-07-08 00:24:45 -0300

    toda a parte de categorias de refeicao feita

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
     # GET /users/new
     def new
-        @users = User.new
+        @user = User.new
     end
 
     # GET /users/1/edit
@@ -24,15 +24,15 @@ class UsersController < ApplicationController
     # POST /meals
     # POST /meals.json
     def create
-        @users = User.new(user_params)
+        @user = User.new(user_params)
 
         respond_to do |format|
-            if @users.save
-                format.html { redirect_to @users, notice: 'User was successfully created.' }
-                format.json { render :show, status: :created, location: @users }
+            if @user.save
+                format.html { redirect_to @user, notice: 'User was successfully created.' }
+                format.json { render :show, status: :created, location: @user }
             else
                 format.html { render :new }
-                format.json { render json: @users.errors, status: :unprocessable_entity }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
             end
         end
     end
@@ -41,14 +41,35 @@ class UsersController < ApplicationController
     # PATCH/PUT /order_meals/1.json
     def update
         respond_to do |format|
-        if @users.update(users_params)
-            format.html { redirect_to @order_meal, notice: 'Order meal was successfully updated.' }
-            format.json { render :show, status: :ok, location: @order_meal }
+        if @user.update(user_params)
+            format.html { redirect_to @user, notice: 'User was successfully updated.' }
+            format.json { render :show, status: :ok, location: @user }
         else
             format.html { render :edit }
-            format.json { render json: @order_meal.errors, status: :unprocessable_entity }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
         end
         end
     end
+
+    # DELETE /meal_categories/1
+    # DELETE /meal_categories/1.json
+    def destroy
+        @user.destroy
+        respond_to do |format|
+        format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+        format.json { head :no_content }
+        end
+    end
+
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_user
+        @user = User.find(params[:id])
+        end
+
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def user_params
+        params.require(:user).permit(:name)
+        end
 
 end
