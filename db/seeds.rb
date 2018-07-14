3a3df426e9f8b03a07a37650d33e8e07bd02b7c9
salanho <espiritosanto.gabriel@gmail.com>
2018-07-13 21:57:51 -0300

    fazendo a parte de pedidos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(:name => 'Administrador', :email => 'admin@admin.com', :password => 'admin123', :password_confirmation => 'admin123', :admin => true)
+Order.create(:price => 72, :User_id => 1, :Situation_id => 1)
+Order.create(:price => 34, :User_id => 1, :Situation_id => 2)
+Order.create(:price => 29, :User_id => 1, :Situation_id => 3)
